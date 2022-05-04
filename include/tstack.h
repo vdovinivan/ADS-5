6da77ff74205fb1ebe874d541a5cfec2f98599c2
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-05-04 15:56:09 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,27 @@
 
 template<typename T, int size>
 class TStack {
-
+ private:
+T a[500];
+int head;
+ public:
+TStack(): head(-1) { }
+T get() const {
+return a[head];
+}
+bool Empty() const {
+return head == -1;
+}
+bool Full() const {
+return head == (size-1);
+}
+void del() {
+if(!Empty())
+head--;
+}
+void push(T value) {
+if(!(Full()))
+a[++head] = value;
+}
 };
 #endif  // INCLUDE_TSTACK_H_
